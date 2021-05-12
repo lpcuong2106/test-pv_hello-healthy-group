4eb8148dd06aeeac9d3fee5ddf6dcd6a20539666
phucuongit <noname21062000@gmail.com>
2021-05-13 00:25:04 +0700

    add procfile

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useCallback, useState } from "react";
 import "./app.scss";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./components/Header";
 import Employee from "./pages/Employee";
 
